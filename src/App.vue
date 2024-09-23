c4bdc2b3c846afc4e6efaaa26900c96f63417276
Islam Ahmed <softeng.islam@gmail.com>
2024-09-23 20:45:24 +0300

    New Updates!

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import DokuHeader from "./components/DokuHeader.vue";
+import DokuHeader from "./components/Header.vue";
 // import DokuList from "./components/DokuList.vue";
 import StatusBar from "./components/StatusBar.vue";
 // import SelectOptions from "./components/SelectOptions.vue";
@@ -22,22 +22,23 @@ div(class="w-full h-11 px-4 flex items-center justify-center bg-[var(--LTheme2)]
 		li(class="w-8 h-8 bg-[var(--LTheme4)] dark:bg-[var(--Theme4)] rounded-md flex items-center justify-center")
 			<i class="ri-folder-transfer-fill ri-lg"></i>
 //- Tags
-div(class="w-full p-2 flex items-center justify-center bg-[var(--LTheme3)] dark:bg-[var(--Theme3)]")
-	ul(class="w-full flex items-center justify-center gap-2 text-gray-400")
-		li(class="text-white") All
+div(class="h-10 w-full pt-2 flex items-center justify-center bg-[var(--LTheme3)] dark:bg-[var(--Theme3)]")
+	ul(class="h-full w-full flex items-center justify-center gap-4 text-gray-500")
+		li(class="h-full py-1 px-2 rounded-t-lg underline font-bold text-white bg-[var(--Theme4)] dark:bg-[var(--Theme4)]") All
 		li(class="") Active
 		li(class="") Completed
 		li(class="") Torrent
 		li(class="") Video
 		li(class="") Music
 
-
-
-div(class="relative left-0 w-full h-[var(--DokuListsHeight)]")
+//- Content
+div(class="relative left-0 w-full h-full overflow-hidden bg-[var(--LTheme3)] dark:bg-[var(--Theme3)]")
 	//- <DokuList/>
-//- <Greet/>
-<StatusBar/>
-div(class="w-96 h-48 p-2 text-center text-lg font-bold text-white rounded-3xl fixed flex gap-5 flex-col items-center justify-center top-[50%] translate-y-[-50%] left-[50%] translate-x-[-50%] border-dashed border-red-200 border-2")
+	//- Download list
+	div(class="w-full h-full flex flex-col items-center justify-center rounded-3xl rounded-b-none bg-[var(--LTheme2)] dark:bg-[var(--Theme2)]")
+		//- <StatusBar/>
+
+div(v-bind:hidden="true" class="w-96 h-48 p-2 text-center text-lg font-bold text-white rounded-3xl fixed flex gap-5 flex-col items-center justify-center top-[50%] translate-y-[-50%] left-[50%] translate-x-[-50%] border-dashed border-red-200 border-2")
 	p Download List is Empty.
 	p(class="text-gray-400") Add new Download URL.
 </template>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,7 +4,7 @@ import { ref } from 'vue';
 
 
 // vars
-let ProjectName = ref<String>('confArch');
+let ProjectName = ref<String>('DoFetcher');
 
 
 function minimize(): void {
