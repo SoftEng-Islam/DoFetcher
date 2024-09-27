b7846700f55ddca668a654ede421d03014412f37
Islam Ahmed <softeng.islam@gmail.com>
2024-09-27 03:51:10 +0300

    New Updates!

--- src/App.vue
+++ src/App.vue
@@ -31,6 +31,21 @@ div(class="h-10 w-full pt-2 flex items-center justify-center bg-[var(--LTheme3)]
 		li(class="") Video
 		li(class="") Music
 
+//- Downloads Information
+ul(class="h-10 w-full flex items-center justify-around gap-3 px-4 border border-gray-500")
+	li(class="h-full flex gap-2 items-center border-r pr-2")
+		input(type="checkbox")
+		span(class="text-white") Select All
+	li(class="h-full flex items-center justify-center border-r pr-2")
+		span(class="text-white") Name
+	li(class="h-full flex items-center border-r pr-2")
+		span(class="text-white") Status
+	li(class="h-full flex gap-2 items-center border-r pr-2")
+		span(class="text-white") Speed
+	li(class="h-full flex gap-2 items-center border-r pr-2")
+		span(class="text-white") Size
+	li(class="h-full flex gap-2 items-center")
+		span(class="text-white") Added
 //- Content
 div(class="relative left-0 w-full h-full overflow-hidden bg-[var(--LTheme3)] dark:bg-[var(--Theme3)]")
 	//- <DokuList/>
