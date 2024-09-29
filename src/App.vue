b21cdade99640b17022afe817b4c8ea73d13f220
Islam Ahmed <softeng.islam@gmail.com>
2024-09-29 15:15:46 +0300

    New Updates!

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import AppPreloader from "./components/AppPreloader.vue";
 import AppHeader from "./components/Header.vue";
 import StatusBar from "./components/StatusBar.vue";
 import AddNewWarn from "./components/AddNewWarn.vue";
@@ -8,6 +9,7 @@ import AppContent from "./components/AppContent.vue";
 </script>
 
 <template lang="pug">
+<AppPreloader/>
 <AddNewWarn/>
 <AppHeader/>
 <TagsBar/>
---
+++ src/components/AppPreloader.vue
@@ -0,0 +1,4 @@
+<script setup lang="ts"></script>
+<template lang="pug">
+div(v-show="false" class="fixed z-10 w-screen h-screen bg-red-500")
+</template>
