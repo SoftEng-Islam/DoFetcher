81f3967aa02e3d6ae0c1623a7169c90023c1f909
Islam Ahmed <softeng.islam@gmail.com>
2024-09-29 18:16:26 +0300

    New Updates!

--- src/components/DownloadsTable.vue
+++ src/components/DownloadsTable.vue
@@ -1,4 +1,23 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+// import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the Data Grid
+// import "ag-grid-community/styles/ag-theme-quartz.css"; // Optional Theme applied to the Data Grid
+// import { AgGridVue } from "ag-grid-vue3"; // Vue Data Grid Component
+
+// Row Data: The data to be displayed.
+const rowData = ref([
+	{ make: "Tesla", model: "Model Y", price: 64950, electric: true },
+	{ make: "Ford", model: "F-Series", price: 33850, electric: false },
+	{ make: "Toyota", model: "Corolla", price: 29600, electric: false },
+]);
+// Column Definitions: Defines the columns to be displayed.
+const colDefs = ref([
+	{ field: "make" },
+	{ field: "model" },
+	{ field: "price" },
+	{ field: "electric" },
+]);
+</script>
 <template lang="pug">
 //- Downloads Information
 ul(class="h-10 w-full flex items-center justify-around gap-3 px-4 border border-gray-500")
@@ -15,4 +34,16 @@ ul(class="h-10 w-full flex items-center justify-around gap-3 px-4 border border-
 		span(class="text-white") Size
 	li(class="h-full flex gap-2 items-center")
 		span(class="text-white") Added
+		<!-- The AG Grid component -->
+//- ag-grid-vue(:rowData="rowData" :columnDefs="colDefs" style="height: 500px" class="ag-theme-quartz")
+div(class="table-container")
+	table(class="custom-table")
+		thead
+			tr
+				th(v-for="column in columns" :key="column") {{ column }}
+		tbody
+			tr(v-for="row in rows" :key="row.name")
+				td(v-for="column in columns" :key="column")
+					//- Dynamically match column with row data
+					{{ row[column.toLowerCase()] }}
 </template>
