8b0966e43d1b13cd899aee6532be3fe88155b50e
Islam Ahmed <softeng.islam@gmail.com>
2024-10-26 19:20:09 +0300

    update

--- src/components/DownloadTask.vue
+++ src/components/DownloadTask.vue
@@ -20,7 +20,7 @@ const toggleSelect = () => {
 
 </script>
 <template lang="pug">
-li(class="relative p-2 w-full h-24 flex items-center rounded-2xl bg-[var(--LTheme4)] dark:bg-[var(--Theme4)] shadow-sm hover:shadow-xl")
+li(class="relative p-2 w-full h-24 flex items-center rounded-2xl border-2 border-[var(--borderFavColor)] dark:border-[var(--LborderFavColor)] bg-[var(--LTheme4)] dark:bg-[var(--Theme4)] shadow-sm hover:shadow-xl")
 	button(v-tippy="{content: 'Select'}" v-on:click="toggleSelect" type="button" class="fixed w-8 h-8 left-1 flex items-center justify-center rounded-full bg-[var(--LTheme4)] dark:bg-[var(--Theme4)]")
 		<i :class="DownList[0].selected? 'ri-checkbox-circle-line' : 'ri-checkbox-blank-circle-line' " class="ri-lg text-[var(--Theme4)] dark:text-[var(--LTheme4)]"></i>
 	//- Image & icon & type of the file
