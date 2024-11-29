6a86af5a699d683d7894ab0ca3e9656097b0c53e
installer <installer@gmail.com>
2024-11-29 13:03:10 +0200

    update

--- default.nix
+++ default.nix
@@ -1,30 +1,29 @@
 let
-	pkgs = import <nixpkgs> { };
+  pkgs = import <nixpkgs> { };
 in
 pkgs.mkShell {
-	nativeBuildInputs = with pkgs; [
-		pkg-config
-		gobject-introspection
-		rustup
-		rust-analyzer
-		cargo
-		cargo-tauri
-		nodejs
-	];
-
-	buildInputs = with pkgs;[
-		at-spi2-atk
-		atkmm
-		cairo
-		gdk-pixbuf
-		glib
-		gtk3
-		harfbuzz
-		librsvg
-		libsoup_3
-		pango
-		webkitgtk
-		webkitgtk_4_1
-		openssl
-	];
+  nativeBuildInputs = with pkgs; [
+    pkg-config
+    gobject-introspection
+    rustup
+    rust-analyzer
+    cargo
+    cargo-tauri
+    nodejs
+  ];
+  buildInputs = with pkgs;[
+    at-spi2-atk
+    atkmm
+    cairo
+    gdk-pixbuf
+    glib
+    gtk3
+    harfbuzz
+    librsvg
+    libsoup_3
+    pango
+    webkitgtk
+    webkitgtk_4_1
+    openssl
+  ];
 }
